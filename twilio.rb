76ccd7f76eae43649d9b1fcdc16b4828dfb0dd9b
smolinsg23 <smolinsam@gmail.com>
2017-01-18 00:45:45 -0600

    Update twilio.rb

--- twilio.rb
+++ twilio.rb
@@ -1,12 +1,12 @@
 require 'twilio-ruby'
 
 # To find these visit https://www.twilio.com/user/account
-account_sid = "AC5514246ab3ee29038c5ce49425d123ea"
-auth_token = "386d158bb16c58248d0412ba8965dd92"
+account_sid = ""
+auth_token = ""
 
 @client = Twilio::REST::Client.new account_sid, auth_token
 from = "+13126354511",
-friends = {'+13125501444' => "sam"}
+friends = {'+5555555' => "sam"}
 friends.each do |key, value|
 message = @client.messages.create(
   :from => from,
